55325d01f98149c200d6b3703befeebdcf3fb180
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-04-23 17:24:45 +0200

    init state and add unique Id

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
+import {uuid} from 'uuidv4';
 
 const Header = ({title}) => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'item1'},
+    {id: uuid(), text: 'item2'},
+    {id: uuid(), text: 'item3'},
+    {id: uuid(), text: 'item4'},
+  ]);
   return (
     <View style={styles.header}>
       <Text style={styles.text}>{title}</Text>
