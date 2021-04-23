23b86a400095d104d037a8dd0acf4721c9fd0a59
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-04-23 15:22:14 +0200

    test react native StyleSheet

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const App = () => {
+  const imageUnsplash = 'https://source.unsplash.com/480x320/?face,portrait';
+
   return (
-    <View>
-      <Text>Hello React Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.largeText}>Hello React Natives</Text>
+      <Image style={styles.image} source={{uri: imageUnsplash}} />
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  largeText: {
+    color: 'darkslateblue',
+    fontSize: 30,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
+});
